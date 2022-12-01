2259e311b2c63e34702a1291f1459aa032a781ca
Patrick Flanigan <patrickflan2001@gmail.com>
2022-12-01 13:39:52 -0500

    working on putting new projects into database

--- React_App/kickstarter/src/Create_Project_Page.js
+++ React_App/kickstarter/src/Create_Project_Page.js
@@ -1,6 +1,14 @@
 import React from "react";
 import {useState} from 'react';
 
+// REPLACE URL BELOW WITH YOURS!
+var base_url = "https://nv069k0pjd.execute-api.us-east-1.amazonaws.com/Prod/";
+
+var create_url = base_url + "createproject";      // POST: {arg1:5, arg2:7}
+
+
+
+
 function Create_Project_Page(){
 
   const [project_name, setproject_name] = useState(null);
@@ -31,10 +39,57 @@ function Create_Project_Page(){
     if(id === "project_deadline"){
       setproject_deadline(value);
   }
+  
+}
+
+function SendtoALambda(project_name, project_story, designer_name, project_genre, fundraising_goal, project_deadline) {
+  var form = document.addForm;
+  var arg1 = project_name;
+  var arg2 = project_story;
+  var arg3 = designer_name;
+  var arg4 = project_genre;
+  var arg5 = fundraising_goal;
+  var arg6 = project_deadline;
+
+  // my actual payload for arg1/arg2
+  var data = {};
+  data["arg1"] = arg1;
+  data["arg2"] = arg2;
+  data["arg3"] = arg3;
+  data["arg4"] = arg4;
+  data["arg5"] = arg5;
+  data["arg6"] = arg6;
+  
+  // to work with API gateway, I need to wrap inside a 'body'
+  var body = {}
+  body["body"] = JSON.stringify(data);
+  var js = JSON.stringify(body);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", create_url, true);
+
+  // send the collected data as JSON
+  //console.log(js);
+  xhr.send(js);
+
+  console.log('Sent Project Informaiton Lambda')
+  // This will process results and update HTML as appropriate. 
+  
+  xhr.onloadend = function () {
+  if (xhr.readyState == XMLHttpRequest.DONE) {
+    console.log('received a status from lambda function')
+    //console.log("response text : ", xhr.responseText);
+    //processAddResponse(xhr.responseText);
+  } else {
+    //processAddResponse("N/A");
+  }
+
+};
 }
 
 const handleSubmit  = () => {
   console.log(project_name,project_story,designer_name,project_genre,fundraising_goal,project_deadline);
+  SendtoALambda(project_name,project_story,designer_name,project_genre,fundraising_goal,project_deadline);
 }
   
   return (
