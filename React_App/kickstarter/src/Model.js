3fb6468764b57f30f467b7ecaecfe02bd39d405c
ajmart14 <alexm9400@gmail.com>
2022-12-06 13:18:24 -0500

    initial commit

--- React_App/kickstarter/src/Model.js
+++ React_App/kickstarter/src/Model.js
@@ -1,15 +1,64 @@
-
-
-
-
 export class Model{
 
-    constructor(user, type){
-        this.user = user;
-        this.type = type;
+    constructor(){
+        this.projects = null;
+        this.admins = null;
+        this.designers = null;
+        this.supporters = null;
         }
+}
 
+export class Project {
 
+    constructor(){
+        this.name = null;
+        this.story = null;
+        this.designer = null;
+        this.goal = null;
+        this.pledges = null;
+        this.supporters = null;
+        this.deadline = null;
+        this.isLaunched = false;
+        this.failed = false;
+    }
+}
+
+export class Designer {
+
+    constructor(){
+    this.email = null;
+    this.password = null;
+    this.projects = null;
+    }
+}
+
+export class Supporter {
+
+    constructor(){
+        this.email = null;
+        this.password = null;
+        this.budget = null;
+        this.pledges = null;
+    }
+}
+
+export class Admin {
+
+    constructor(){
+        this.email = null;
+        this.password = null;
+        this.projects = null;
+    }
+}
+
+export class Pledge {
+
+    constructor(){
+        this.amount = null;
+        this.reward = null;
+        this.maxsup = 0;
+        this.currentsup = 0;
+    }
 }
 
 export default Model;
